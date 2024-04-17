a4194044fcc435d501f3a3012d2cdf2044f4d040
Cristian Paredes <csparedes@pucesi.edu.ec>
2024-04-17 11:44:39 -0500

    ux-ui: footer done

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,20 +1,75 @@
 <template>
-    <div class="w-full h-max-481 bg-footer-bg grid grid-cols-3">
-        <div>
-            <p class="text-white">
-                Sobre nosotros
+    <div class="w-full h-max-481 bg-footer-bg grid grid-cols-1 md:grid-cols-3">
+        <div class="p-8">
+            <p class="text-footer-title-text text-xl font-monserrat tracking-widest ">
+                SOBRE NOSOTROS
             </p>
-            <p class="text-white">
-                Aprehenser desarrolla y responde a la planificación estratégica anual, cuya misión estará siempre direccionada en el mejoramiento de la calidad educativa, mediante estrategias de innovación, acompañamiento, participación y capacitación docente
-            </p>
+            <div class="pt-12">
+                <p class="text-white text-lg text-left font-poppins tracking-wider">
+                    Aprehenser desarrolla y responde a la planificación estratégica anual, cuya misión estará siempre
+                    direccionada en el mejoramiento de la calidad educativa, mediante estrategias de innovación,
+                    acompañamiento, participación y capacitación docente.
+                </p>
+            </div>
+            <div class="flex gap-x-12 pt-8">
+                <a href=""><img src="../assets/svg/facebook-icon.svg" alt="facebook-icon"></a>
+                <a href=""><img src="../assets/svg/instagram-icon.svg" alt="instagram-icon"></a>
+            </div>
         </div>
-        <div>
-            <p class="text-white">Convocatorias</p>
-            <p class="text-white">Revistas</p>
-            <p class="text-white">Nuestras noticias</p>
+        <div class="p-8 my-auto flex-col gap-8">
+            <div class="flex">
+                <img src="../assets/svg/arrow-right-blue.svg" alt="">
+                <p class="text-white">Convocatorias</p>
+            </div>
+            <div class="border-b border-divider-footer my-4 px-4"></div>
+            <div class="flex">
+                <img src="../assets/svg/arrow-right-blue.svg" alt="">
+                <p class="text-white">Revistas</p>
+
+            </div>
+            <div class="border-b border-divider-footer my-4 px-4"></div>
+            <div class="flex">
+                <img src="../assets/svg/arrow-right-blue.svg" alt="">
+                <p class="text-white">Nuestras noticias</p>
+            </div>
+            <div class="border-b border-divider-footer my-4 px-4"></div>
+            <div class="py-8 px-2">
+                <img class="mx-auto" src="../assets/svg/aprehenser-logo-footer.svg" alt="aprehenser-logo-footer">
+            </div>
         </div>
-        <div>
-            <p class="text-white">Contactos</p>
+        <div class="p-8">
+            <p class="text-footer-title-text text-xl font-monserrat tracking-widest ">CONTACTOS</p>
+            <div class="pt-8">
+                <div class="flex gap-4">
+                    <img src="../assets/svg/pointer-location-map.svg" alt="pointer-location">
+                    <p class="font-poppins text-white">Dirección: </p>
+                </div>
+                <div class="pl-6 pt-2">
+                    <p class="font-poppins text-white">Av. Jorge Guzmán Rueda y Av. Aurelio Espinosa Pólit. ciudadela
+                        "La Victoria", Ecuador</p>
+                </div>
+            </div>
+            <div class="border-b border-divider-footer my-4 px-4"></div>
+            <div>
+                <div class="flex gap-4">
+                    <img src="../assets/svg/phone-contact.svg" alt="phone-contact">
+                    <p class="font-poppins text-white">Teléfono:</p>
+                </div>
+                <div class="pl-8 pt-2">
+                    <p class="font-poppins text-white">(593) 06 2615 500 ext. 2057</p>
+                </div>
+            </div>
+            <div class="border-b border-divider-footer my-4 px-4"></div>
+            <div>
+                <div class="flex gap-4">
+                    <img src="../assets/svg/mail-contact.svg" alt="mail-contact">
+                    <p class="font-poppins text-white">Correo:</p>
+                </div>
+                <div class="pl-8 pt-2">
+                    <p class="font-poppins text-white">aprehenser@pucesi.edu.ec</p>
+                </div>
+            </div>
+            <div class="border-b border-divider-footer my-4 px-4"></div>
         </div>
     </div>
 </template>
@@ -23,6 +78,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,14 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Roboto', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'sans-serif']
+        'sans': ['Roboto', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+        'monserrat': ['Montserrat', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+        'poppins': ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
       },
       colors:{
         'footer-bg': '#141D28',
+        'divider-footer': '#343D48',
+        'footer-title-text': '#11BFDD'
       }
     },
   },
