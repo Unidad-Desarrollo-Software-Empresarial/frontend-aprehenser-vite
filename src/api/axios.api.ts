1d912dde5d53ee7b0653cf2390906325ec3cc0d5
Cristian Paredes <csparedes@pucesi.edu.ec>
2024-05-22 10:15:04 -0500

    fix: cors solucionado

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: "https://aprehenser.pucesi.edu.ec/index.php/api",
+    // withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    }
 })
