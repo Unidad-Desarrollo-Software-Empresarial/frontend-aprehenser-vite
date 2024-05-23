3ac726eded892854ee05d713b3854683e54d1c19
Cristian Paredes <csparedes@pucesi.edu.ec>
2024-05-23 09:43:20 -0500

    progress

--- src/modules/home/pages/DesarrolloFormacionPorAmbitoIdPage.vue
+++ src/modules/home/pages/DesarrolloFormacionPorAmbitoIdPage.vue
@@ -17,6 +17,36 @@
                 </div>
             </div>
 
+
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tema</th>
+                            <th>Duración</th>
+                            <th>Itinerario</th>
+                            <th>Fecha Inicio</th>
+                            <th>Fecha Fin</th>
+                            <th>Estado Id</th>
+                            <th>Tipo</th>
+                            <th>Imagen</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="curso in ambitoDetalle?.cursos" :key="curso.id">
+                            <td>{{ curso.tema }}</td>
+                            <td>{{ curso.duracion }}</td>
+                            <td>{{ curso.itinerario }}</td>
+                            <td>{{ curso.inicio }}</td>
+                            <td>{{ curso.fin }}</td>
+                            <td>{{ curso.estadoId }}</td>
+                            <td>{{ curso.tipo }}</td>
+                            <td>{{ curso.imagen }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     </MainLayout>
 </template>
@@ -29,6 +59,10 @@ import { onMounted, ref } from 'vue';
 import { api } from '@/api/axios.api';
 import SpinnerComponent from '../components/SpinnerComponent.vue';
 import { DetalleFormacionByIdResponseInterface } from '../interfaces/detalle-formacion-by-id-response.interface';
+import { createColumnHelper } from '@tanstack/vue-table';
+
+
+
 
 
 const route = useRoute()
@@ -45,6 +79,13 @@ onMounted(async () => {
     }
     ambitoDetalle.value = response.data
 })
+
+const columnHelper = createColumnHelper()
+
+const columns = [ 
+    columnHelper.display({id: 'tema'}),
+]
+
 
 </script>
 
